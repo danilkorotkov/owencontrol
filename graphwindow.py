f72e8e1f3be5fc4e44e83bc06117b151b40e5172
danilkorotkov <spballiance@gmail.com>
2017-05-09 13:11:10 +0300

    pep8

--- graphwindow.py
+++ graphwindow.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 from operator import methodcaller
 import sys, os, time, string
-from PyQt4 import QtCore, QtGui, uic 
+from PyQt4 import QtCore, QtGui, uic
 from PyQt4.Qt import Qt
-from PyQt4.QtGui import *
-#from PyQt4.QtCore import QObject, SIGNAL
 import numpy as np
 import pyqtgraph as pg
 
-MainInterfaceWindow = "graphwindow.ui" 
+MainInterfaceWindow = "graphwindow.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(MainInterfaceWindow)
 
-DEGREE  = u"\u00B0" + 'C'
+DEGREE = u"\u00B0" + 'C'
 
-class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
+
+class GraphWindow(QtGui.QMainWindow, Ui_MainWindow):
     """MainWindow inherits QMainWindow"""
-    path="logs/"
-    lf1=[]
+    path = "logs/"
+    lf1 = []
 
-    def __init__ ( self, parent = None ):
+    def __init__(self, parent=None):
         super(GraphWindow, self).__init__(parent)
         Ui_MainWindow.__init__(self)
         pg.setConfigOption('background', (194, 194, 194))
@@ -41,61 +40,59 @@ class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
         self.sceneH = int(self.graphicsView.height())
         self.graphicsView.addLegend(size=None, offset=(self.sceneW * 0.77, self.sceneH * 0.22))
         self.searchLogs()
- 
-    def __del__ ( self ):
-        self.ui = None
 
+    def __del__(self):
+        self.ui = None
 
     def exit(self):  # Funkzija zavershenija raboti okna.
         self.close()
-        
 
-    def searchLogs(self): # Funkzija vivoda imen failov v spisok
-        
-        ld=os.listdir(self.path)   # Berem spisok failov  directorija
+    def searchLogs(self):  # Funkzija vivoda imen failov v spisok
 
-        lf=[]                           # Fil'truem faili s rasshireniem .txt - log faili
+        ld = os.listdir(self.path)  # Berem spisok failov  directorija
+
+        lf = []  # Fil'truem faili s rasshireniem .txt - log faili
         for i in range(len(ld)):
             if ld[i].endswith(".txt"):
                 lf.append(ld[i])
 
-        lf.sort()      # сортировка по времени
-        lf.reverse()   # в обратном порядке
-        
-        self.lf1=[]
+        lf.sort()  # сортировка по времени
+        lf.reverse()  # в обратном порядке
+
+        self.lf1 = []
         for i in range(len(lf)):
             self.lf1.append([])
             self.lf1[i].append(lf[i])
-            t=lf[i].split('_')[0]
-            s=time.localtime(float(t))
-            l=lf[i].split('_')[1]
-            if l=="1":
-               l=u"6,5 м"
+            t = lf[i].split('_')[0]
+            s = time.localtime(float(t))
+            l = lf[i].split('_')[1]
+            if l == "1":
+                l = u"6,5 м"
             else:
-               l=u"3,5 м" 
-            
-            if s[4]<10:
-                minutes='0'+str(s.tm_min)
-            else:
-                minutes=str(s.tm_min)
-            s='%s-%s-%s %s:%s' % (str(s.tm_year)[2:],str(s.tm_mon),str(s.tm_mday),str(s.tm_hour),minutes)+u" Линия: "+l
-            self.lf1[i].append(s)
-        
+                l = u"3,5 м"
 
-        for i in range(len(lf)):    # Pomeschaem imena log failov v vizual'nij spisok
+            if s[4] < 10:
+                minutes = '0' + str(s.tm_min)
+            else:
+                minutes = str(s.tm_min)
+            s = '%s-%s-%s %s:%s' % (
+            str(s.tm_year)[2:], str(s.tm_mon), str(s.tm_mday), str(s.tm_hour), minutes) + u" Линия: " + l
+            self.lf1[i].append(s)
+
+        for i in range(len(lf)):  # Pomeschaem imena log failov v vizual'nij spisok
             self.listWidget.addItem(self.lf1[i][1])
 
-
-    def letsgo(self): # выбор файла в списке
-        self.statusBar.showMessage(u"Имя лога: "+self.lf1[self.listWidget.currentRow()][0]) # Vivodim coobschenie v statusBar.
+    def letsgo(self):  # выбор файла в списке
+        self.statusBar.showMessage(
+            u"Имя лога: " + self.lf1[self.listWidget.currentRow()][0])  # Vivodim coobschenie v statusBar.
         self.draw()  # Vizivaem funkziju prorisovki grafika
 
     def draw(self):  # рисуем график
 
         file_name = self.lf1[self.listWidget.currentRow()][0]  # параметр функции - имя лог файла
 
-        ust=file_name.split('_')[-1] # Выделение уставки из имени файла
-        ust=int(ust.split('.')[0])
+        ust = file_name.split('_')[-1]  # Выделение уставки из имени файла
+        ust = int(ust.split('.')[0])
 
         try:  # читаем лог файл
             with open(self.path + file_name) as f:
@@ -104,7 +101,7 @@ class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
             self.graphicsView.setTitle(title=u'Ошибка чтения файла')
             return
 
-        if lines[-1]=='':
+        if lines[-1] == '':
             lines.pop()
         else:
             pass
@@ -113,12 +110,12 @@ class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
 
         cpw = list(map(methodcaller("split", ","), lines))
 
-        timeAxis=[] # Заполнение массива координат графиков
-        tempLine=[]
-        powerLine=[]
-        stateLine=[]
-        fanLine=[]
-        heatLine=[]
+        timeAxis = []  # Заполнение массива координат графиков
+        tempLine = []
+        powerLine = []
+        stateLine = []
+        fanLine = []
+        heatLine = []
         try:
             for i in range(len(cpw)):
                 timeAxis.append(float(cpw[i][0]))
@@ -130,62 +127,65 @@ class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
         except ValueError:
             self.graphicsView.setTitle(title=u'Ошибка')
             return
-        length=len(timeAxis)
-        delt=0
-        x0=timeAxis[0]
-        x1=timeAxis[-1]
-        iter=0
+        length = len(timeAxis)
+        delt = 0
+        x0 = timeAxis[0]
+        x1 = timeAxis[-1]
+        itera = 0
         try:
-            while delt==0:
-                if stateLine[iter]==1:
-                    delt=timeAxis[iter]
-                    index=iter
-                if iter<length:
-                    iter+=1
+            while delt == 0:
+                if stateLine[itera] == 1:
+                    delt = timeAxis[itera]
+                    index = itera
+                if itera < length:
+                    itera += 1
         except IndexError:
-            delt=delt=timeAxis[-1]
-            index=-1
-        cpw=[] # clear memory
-        lines=[]
+            delt = delt = timeAxis[-1]
+            index = -1
+        cpw = []  # clear memory
+        lines = []
 
-        dx=x1-delt
+        dx = x1 - delt
 
-        for i in range(length): # Вычитание нулевой точки по оси x
-            timeAxis[i]=timeAxis[i]-x0
+        for i in range(length):  # Вычитание нулевой точки по оси x
+            timeAxis[i] = timeAxis[i] - x0
 
-        timeAxis=np.array(timeAxis)
+        timeAxis = np.array(timeAxis)
         tempLine = np.array(tempLine)
         powerLine = np.array(powerLine)
         stateLine = np.array(stateLine)
         fanLine = np.array(fanLine)
         heatLine = np.array(heatLine)
         ustLine = np.arange(length)
-        ustLine[::]=ust
+        ustLine[::] = ust
 
-        maxTemp=tempLine.max() # поиск максимумов координат графиков
-        maxPower=powerLine.max()
-        maxHeat=heatLine.max()
+        maxTemp = tempLine.max()  # поиск максимумов координат графиков
+        maxPower = powerLine.max()
+        maxHeat = heatLine.max()
 
-        minTemp=tempLine.min()
+        minTemp = tempLine.min()
         minHeat = heatLine.min()
 
         minimum = minTemp
         maximum = ust
 
-        if minTemp > minHeat: minimum = minHeat
+        if minTemp > minHeat:
+            minimum = minHeat
 
-        if maximum < maxHeat: maximum = maxHeat
-        elif maximum < maxTemp: maximum = maxTemp
+        if maximum < maxHeat:
+            maximum = maxHeat
+        elif maximum < maxTemp:
+            maximum = maxTemp
 
-        timeAxis = timeAxis/60
+        timeAxis /=  60
 
         self.graphicsView.setLimits(xMin=0, xMax=timeAxis[-1], yMin=minimum, yMax=maximum)
 
         if maxPower != 0:
-            powerLine=(maximum-minimum)*powerLine/(maxPower*2) + minimum # Нормировка на 1 и далее нормировка на примерно половину высоты графика температуры
-        stateLine=(maximum-minimum)*stateLine/2.1 + minimum
-        fanLine=(maximum-minimum)*fanLine/2.2 + minimum
-
+            powerLine = (maximum - minimum) * powerLine / (
+            maxPower * 2) + minimum  # Нормировка на 1 и далее нормировка на примерно половину высоты графика температуры
+        stateLine = (maximum - minimum) * stateLine / 2.1 + minimum
+        fanLine = (maximum - minimum) * fanLine / 2.2 + minimum
 
         self.graphicsView.clear()
         try:
@@ -194,9 +194,9 @@ class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
             pass
 
         labelStyle = {'color': '#000', 'font-size': '12pt'}
-        self.graphicsView.setLabel('left', u'Температура, '+DEGREE, **labelStyle)
+        self.graphicsView.setLabel('left', u'Температура, ' + DEGREE, **labelStyle)
         self.graphicsView.setLabel('bottom', u'Время, мин', **labelStyle)
-        #self.graphicsView.getAxis('bottom').setStyle(tickTextOffset=300)
+        # self.graphicsView.getAxis('bottom').setStyle(tickTextOffset=300)
 
         self.graphicsView.showGrid(x=True, y=True, alpha=1)
 
@@ -215,38 +215,38 @@ class GraphWindow (QtGui.QMainWindow, Ui_MainWindow):
         # self.graphicsView.addLine(y=maxHeat, name=self.SetInfoPanelText('ТЭН'), pen=pg.mkPen('y', width=3))
         # self.graphicsView.addLine(x=timeAxis[index], name=self.SetInfoPanelText('Выдержка'), pen=pg.mkPen('m', width=3))
 
-        delay=dx//60
-        delay=int(delay%60)
-        delay=str(delay) +' мин' # Выдержка
+        delay = dx // 60
+        delay = int(delay % 60)
+        delay = str(delay) + ' мин'  # Выдержка
 
-        s=time.localtime(float(x0))
-        m=str(s[4])
-        if len(m)==1:
-            m='0'+m
-        h=str(s[3])
-        if len(h)==1:
-            h='0'+h
-        t_start=h+':'+m # Начало
+        s = time.localtime(float(x0))
+        m = str(s[4])
+        if len(m) == 1:
+            m = '0' + m
+        h = str(s[3])
+        if len(h) == 1:
+            h = '0' + h
+        t_start = h + ':' + m  # Начало
 
-        s=time.localtime(float(x1))
-        m=str(s[4])
-        if len(m)==1:
-            m='0'+m
-        h=str(s[3])
-        if len(h)==1:
-            h='0'+h
-        t_end=h+':'+m # # Окончание
+        s = time.localtime(float(x1))
+        m = str(s[4])
+        if len(m) == 1:
+            m = '0' + m
+        h = str(s[3])
+        if len(h) == 1:
+            h = '0' + h
+        t_end = h + ':' + m  # # Окончание
 
         self.graphicsView.setTitle(title=self.SetInfoPanelText(
             'Начало: ' + t_start + ', Окончание: ' + t_end + ', Выдержка: ' + delay + ', ТЭН: ' + str(
                 maxHeat)))
 
-
-    def SetInfoPanelText (self,text):
+    def SetInfoPanelText(self, text):
         out = _translate("GraphWindow",
                          "<body style=\" font-family:\'Free Helvetian\'; font-size:14pt;\"><p>%s</p></body>" % text,
                          None)
         return out
+
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -256,8 +256,10 @@ except AttributeError:
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
+
+
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)  
+        return QtGui.QApplication.translate(context, text, disambig)
